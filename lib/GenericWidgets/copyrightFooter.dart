357f0c0157c8dd913b2db91cf2c5e8bba3d98abc
daviiid99 <davidmolinamorales@gmail.com>
2023-04-07 12:07:30 +0200

    small changes

--- lib/GenericWidgets/copyrightFooter.dart
+++ lib/GenericWidgets/copyrightFooter.dart
@@ -11,7 +11,7 @@ class CopyrightFooter extends StatelessWidget{
       width: double.maxFinite,
       height: height * 0.1 ,
       margin: EdgeInsets.only(left: width * 0.1, right: width * 0.1, top: height * 0.9, bottom: height * 0.05),
-      child: const Text("©1980-2023 Mattel | ©2023 daviiid99", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20), textAlign: TextAlign.center,),
+      child: const Text("©2023 daviiid99", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20), textAlign: TextAlign.center,),
     );
   }
 }
