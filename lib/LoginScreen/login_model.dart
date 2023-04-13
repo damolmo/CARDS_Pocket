ce832e520e21e6695ac87c3540ee37c84861fae2
daviiid99 <davidmolinamorales@gmail.com>
2023-04-13 21:12:59 +0200

    Initial login screen with account creation and login into account

---
+++ lib/LoginScreen/login_model.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:stacked/stacked.dart';
+import '../Screens/screens.dart';
+
+class LoginModel extends BaseViewModel implements Initialisable{
+
+  TextEditingController userNameController = TextEditingController(text: "");
+  TextEditingController userPasswordController = TextEditingController(text: "");
+  String fieldNameError = "";
+  String fieldPasswordError = "";
+  List<Accounts> accounts  = [];
+  bool existingAccounts = false;
+  bool allFieldsCompleted = false;
+  bool loginNamePassed = false;
+  bool loginPassWordPassed = false;
+  bool loginSucceed = false;
+
+  @override
+  void initialise(){
+    getExistingAccounts(); // First of all, get ALL accounts
+  }
+
+
+  getExistingAccounts() async {
+    // This method get access to our existing database and ask for accounts
+    try{
+      Accounts.createAccountsTable();
+      accounts = await Accounts.retrieveAccountEntries();
+      notifyListeners();
+    } catch (e){
+      print(e);
+    }
+
+    setLoginScreenAttributes(); // After retrieve, check box length
+
+  }
+
+  setLoginScreenAttributes() async {
+    // Set current login screen titles and subtitles according to db
+
+    if (accounts.isEmpty){
+      // No accounts
+      existingAccounts = false;
+    } else {
+      existingAccounts = true;
+    }
+  }
+
+  fieldsErrorControls() async {
+    // Check fields empty
+
+    if (userNameController.text.isEmpty) fieldNameError = "El campo usuario es obligatorio";
+    if (userPasswordController.text.isEmpty) fieldPasswordError = "El campo contraseña es obligatorio";
+    if(userPasswordController.text.isNotEmpty && userNameController.text.isNotEmpty) allFieldsCompleted = true;
+    notifyListeners();
+  }
+
+  loginIntoAccount(){
+    // A method that checks if current user + password is correct
+    // Navigates to next screen if succeed
+    fieldsErrorControls();
+    int occurrences  = 0;
+
+    if (allFieldsCompleted){
+      for (Accounts account in accounts){
+        if (!loginSucceed){
+          if (account.userName == userNameController.text) {
+            // User detected
+            loginNamePassed = true;
+            occurrences = 0;
+            fieldNameError = "";
+            occurrences ++;
+            notifyListeners();
+          } else if (occurrences == 0) {
+            loginNamePassed = false;
+            fieldNameError = "Esta cuenta no existe";
+            notifyListeners();
+          }
+
+            if (account.userPassword == userPasswordController.text){
+              // Password verified
+              loginPassWordPassed = true;
+              loginSucceed = true;
+              notifyListeners();
+            } else {
+              // Password wrongr
+              loginPassWordPassed = false;
+              fieldPasswordError = "La contraseña no coincide";
+              notifyListeners();
+            }
+        }
+
+      }
+    }
+
+    if (allFieldsCompleted && !loginNamePassed){
+      // The account doesn't exists in current database
+      fieldNameError = "Esta cuenta no existe";
+      notifyListeners();
+    }
+  }
+
+  createAccount(){
+    // A method that creates an account if doesn't exists
+
+    loginNamePassed = false;
+    loginPassWordPassed = false;
+    notifyListeners();
+
+    fieldsErrorControls();
+
+    if (allFieldsCompleted){
+      if (accounts.isNotEmpty){
+        for (Accounts account in accounts){
+          if (account.userName == userNameController.text){
+            loginNamePassed = true;
+            fieldNameError = "Cuenta existente. Inicia sesión";
+            notifyListeners();
+          }
+        }
+      }
+    }
+
+    if (!loginNamePassed){
+      // Account doesn't exist
+      // Time to add it
+      Accounts newAccount = Accounts(userName: userNameController.text, userPassword: userPasswordController.text);
+      Accounts.insertAccountEntry(newAccount);
+      loginSucceed = true;
+      notifyListeners();
+    }
+
+  }
+
+
+
+}
---
+++ lib/LoginScreen/widgets/loginForm.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:uno_pocket/LoginScreen/login_model.dart';
+import 'package:uno_pocket/Screens/screens.dart';
+
+class LoginForm extends StatelessWidget{
+  @override
+  const LoginForm({
+    required this.viewModel,
+    super.key
+});
+
+  final LoginModel viewModel;
+
+  @override
+  Widget build(BuildContext context){
+
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: double.maxFinite,
+      height: height * 0.62,
+      margin: EdgeInsets.only(top: height * 0.25, bottom: height * 0.05, left: width * 0.05, right: width * 0.05),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [BoxShadow(color: Colors.redAccent.shade700, offset: Offset.zero, blurRadius: 3.0, blurStyle: BlurStyle.normal )]
+      ),
+      child: Column(
+        children: [
+          // Form Title
+          FormTitle(viewModel: viewModel),
+
+          // Form Fields
+          FormFields(viewModel: viewModel),
+
+          // Form Button
+          FormButton(viewModel: viewModel,),
+
+        ],
+      ),
+
+    );
+  }
+}
+
+class FormTitle extends StatelessWidget{
+  @override
+  const FormTitle({
+    required this.viewModel,
+    super.key});
+
+  final LoginModel viewModel;
+
+  @override
+  Widget build(BuildContext context){
+
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: double.maxFinite,
+      height: height * 0.1,
+      margin: EdgeInsets.only(top: height * 0.02, bottom: height * 0.05, left: width * 0.1, right: width * 0.1),
+      child: Text(viewModel.existingAccounts ? "Iniciar Sesión" : "Crear Cuenta", style: TextStyle(color: Colors.black, fontSize: width * 0.1, fontWeight: FontWeight.bold, ), textAlign: TextAlign.center, ),
+    );
+  }
+}
+
+class FormFields extends StatelessWidget{
+  @override
+  const FormFields({
+    required this.viewModel,
+    super.key,
+});
+
+  final LoginModel viewModel;
+
+  @override
+  Widget build (BuildContext context){
+
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: double.maxFinite,
+      height: height * 0.30,
+      margin: EdgeInsets.only( left: width * 0.05, right: width * 0.05),
+      child: Column(
+        children: [
+          // UserName Field
+          Container(
+            width: double.maxFinite,
+            height: height * 0.12,
+            margin: EdgeInsets.only(bottom: height * 0.01, left: width * 0.02, right: width * 0.02),
+            child: TextField(
+              style: const TextStyle(fontSize: 25),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Introduce un usuario",
+                hintStyle: const TextStyle(color: Colors.blueGrey, fontSize: 20),
+                errorText: viewModel.fieldNameError,
+                errorStyle: const TextStyle(color: Colors.red, fontSize: 12),
+                icon: const Icon(Icons.face_rounded, color: Colors.blue,),
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              controller: viewModel.userNameController,
+              keyboardType: TextInputType.name,
+            ),
+          ),
+
+          // PassWord Field
+          Container(
+            width: double.maxFinite,
+            height: height * 0.12,
+            margin: EdgeInsets.only(left: width * 0.02, right: width * 0.02, bottom: height * 0.05),
+            child: TextField(
+              style: const TextStyle(fontSize: 25),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Introduce una contraseña",
+                errorText: viewModel.fieldPasswordError,
+                errorStyle: const TextStyle(color: Colors.red, fontSize: 12),
+                hintStyle: const TextStyle(color: Colors.blueGrey, fontSize: 20),
+                icon: const Icon(Icons.lock_rounded, color: Colors.blue,),
+                filled: true,
+                fillColor: Colors.white
+              ),
+              controller: viewModel.userPasswordController,
+              keyboardType: TextInputType.visiblePassword,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget{
+  @override
+  const FormButton({
+    required this.viewModel,
+    super.key});
+
+  final LoginModel viewModel;
+
+  @override
+  Widget build(BuildContext context){
+
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: double.maxFinite,
+      height: height * 0.07,
+      margin: EdgeInsets.only(bottom: height * 0.05, left: width * 0.1, right: width * 0.1),
+      child: ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+        child : TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.blueAccent
+          ),
+          onPressed : (){
+            viewModel.existingAccounts ?  viewModel.loginIntoAccount() :  viewModel.createAccount();
+            if (viewModel.loginSucceed ) {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const TitleView()));
+            }
+          },
+          child: Text(viewModel.existingAccounts ? "Iniciar" : "Registrar", style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25), textAlign: TextAlign.center,),
+        ),
+      ),
+    );
+  }
+}
